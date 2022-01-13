b42ab251d4449e25cad60998981eec1b2de3f5fe
Elvan H <elvanhd@gmail.com>
2022-01-13 21:27:21 +0700

    creating a redux store for react

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux';
+
+const initialCounter = {
+  counter: 0,
+};
+
+const counterReducer = (state = initialCounter, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1,
+      };
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return initialCounter;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
